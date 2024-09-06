f44ff6dc37ae37d21cfa64cc91c9beb3d5d972e4
hire2inspire2023 <info@hire2inspire.com>
2024-09-07 00:13:59 +0530

    file size

--- utils/pdfUpload.js
+++ utils/pdfUpload.js
@@ -7,6 +7,7 @@ const acceptedFileTypes = [
 const upload = multer({ 
     // dest: 'uploads/',
     storage: multer.memoryStorage(),
+    limits: { fileSize: 5 * 1024 * 1024 },
     fileFilter: (req, file, cb) => {
         if (!acceptedFileTypes.includes(file.mimetype)) {
           return cb(new Error('Only Pdf Allowed!'))
